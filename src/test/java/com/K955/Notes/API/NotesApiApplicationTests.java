a1c747f334092d7e9d32ce8b944041ecd5104edb
krishsharma955 <krishsharma9005@gmail.com>
2026-06-19 12:05:26 +0530

    feat: Project Setup

---
+++ src/test/java/com/K955/Notes/API/NotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.K955.Notes.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
